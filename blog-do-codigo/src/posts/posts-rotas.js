3b871b9ee612bfbfefd32f768748ef1e0c87608a
Gustavo Henrique <gustavo._henrique@hotmail.com>
2021-04-27 21:22:09 -0300

    Upload do Projeto.

---
+++ blog-do-codigo/src/posts/posts-rotas.js
@@ -0,0 +1,14 @@
+const postsControlador = require('./posts-controlador');
+const passport = require('passport')
+const {middlewareAutenticacao} = require('../usuarios')
+
+module.exports = app => {
+  app
+    .route('/post')
+    .get(postsControlador.lista)
+    .post(
+
+      middlewareAutenticacao.bearer,
+      postsControlador.adiciona
+    );
+};
